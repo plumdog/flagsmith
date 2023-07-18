d4b80b9fb37866fd661a5bdc2ae017e7a3db74a7
Andrew Plummer <andrew.plummer@isotoma.com>
2023-07-18 11:46:45 +0100

    Minimal changes such that pre-commit linting passes

--- api/app/settings/local.py
+++ api/app/settings/local.py
@@ -1,18 +1,18 @@
-from app.settings.common import *
+from app.settings.common import *  # noqa: F403
 
 ENABLE_AXES = False
 
 
-ALLOWED_HOSTS.extend([".ngrok.io", "127.0.0.1", "localhost"])
+ALLOWED_HOSTS.extend([".ngrok.io", "127.0.0.1", "localhost"])  # noqa: F405
 
-INSTALLED_APPS.extend(["debug_toolbar"])
+INSTALLED_APPS.extend(["debug_toolbar"])  # noqa: F405
 
-MIDDLEWARE.extend(["debug_toolbar.middleware.DebugToolbarMiddleware"])
+MIDDLEWARE.extend(["debug_toolbar.middleware.DebugToolbarMiddleware"])  # noqa: F405
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
-SWAGGER_SETTINGS["USE_SESSION_AUTH"] = False
+SWAGGER_SETTINGS["USE_SESSION_AUTH"] = False  # noqa: F405
 
 # Allow admin login with username and password
 ENABLE_ADMIN_ACCESS_USER_PASS = True
--- api/app/settings/production.py
+++ api/app/settings/production.py
@@ -1,3 +1,3 @@
-from app.settings.common import *
+from app.settings.common import *  # noqa: F403
 
-REST_FRAMEWORK["PAGE_SIZE"] = 999
+REST_FRAMEWORK["PAGE_SIZE"] = 999  # noqa: F405
